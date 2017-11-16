e975531bbf971058266fb04cac2f3635adfa5917
George Redinger <george.redinger@cloudspindle.com>
2017-11-16 19:25:43 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ for (file in files) {
     });
     fs.writeFileSync(`raw4_${files[file]}.csv`, raw4.join(''), 'utf-8');
     
-  //   plot(`smooth_${files[file]}.csv`);
-  //   plot(`raw4_${files[file]}.csv`);
+   plot(`smooth_${files[file]}.csv`);
+   plot(`raw4_${files[file]}.csv`);
 
     let mean =  M.reduce((a,b) => a+b,0)/M.length;
     let steps = M.map((val,index,arr) => {
@@ -91,7 +91,7 @@ for (file in files) {
            return(`"${ts}",${val},${step}\n`);
     });
     fs.writeFileSync(`steps.csv`, steps.join(''), 'utf-8');
-    // plot('steps.csv');
+     plot('steps.csv');
     break;
 
 }
